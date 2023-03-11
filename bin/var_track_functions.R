1bd4009a5078a5a4d441266d06035d7c506a034a
Iñaki Sasiain Casado <inakisasiainca@gmail.com>
2023-03-11 12:38:26 +0100

    Transform some of the R scripts into functins in the var_track_functions.R scripy

--- bin/var_track_functions.R
+++ bin/var_track_functions.R
@@ -1,5 +1,6 @@
 #!/usr/bin/env Rscript
 
+#This functions detects the pathogenic variants from the individual input bim file
 path_var_detector <- function(clinvar_ref_file, bim_file) {
   
   # Reading the clinvar variant files and bim files as dataframes
@@ -36,4 +37,55 @@ path_var_detector <- function(clinvar_ref_file, bim_file) {
     }
     return(ret_ls)
   }
+}
+
+#Creating a function to plot the MAF in a map
+plotting_MAF_in_map <- function(MAF_matrix, coordinates_file, variant_of_interest) {
+    #Loading the required data
+    library(ggplot2)
+    library(rworldmap)
+
+    #Reading the files
+    pop_df <- read.csv(coordinates_file, sep =",")
+    MAF_df <- read.table(MAF_matrix)
+
+    #Getting the MAF of out variant of interest and adding it to the dataframe
+    pop_df$"Minor allele frequency" <- t(MAF_df[variant_of_interest,])
+    pop_df$"Mayor allele frequency" <- 1 - pop_df$"Minor allele frequency"
+
+    #Getting the coordenates to limit the map
+    long <- c(min(pop_df$Longitude) - 1, max(pop_df$Longitude) + 1)
+    lat <- c(min(pop_df$Latitude) - 1, max(pop_df$Latitude) + 1)
+
+    #Potting the MAF in a map
+    png("/home/inf-52-2022/pop_gen_project/05_Generating_plots/my_plot.png", width=700, height=700)
+    my_plot <- mapPies(pop_df, nameX = "Longitude", nameY = "Latitude",
+            nameZs = c("Minor allele frequency", "Mayor allele frequency"), 
+            zColours = c("Red", "Green"),
+            symbolSize = 1, 
+            xlim = long, 
+            ylim = lat, 
+            borderCol = "Black",
+            landCol = "lightGrey",
+            oceanCol = "lightBlue",
+            lwd=2,
+            addSizeLegend=FALSE,
+            addCatLegend=FALSE)
+
+    #Creating a legend for the plot
+    legend(long[1],lat[1]+4,
+       legend=c("Minor allele frequency",
+                 "Mayor allele frequency"),
+       col=c("red",
+             "green"),
+       pch=16,
+       cex=1.7,
+       pt.cex=2,
+       bty="o",
+       box.lty=2,
+       horiz = F,
+       bg="#FFFFFF70")
+
+    dev.off()
+
 }
