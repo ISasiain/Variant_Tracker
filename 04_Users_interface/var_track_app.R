3337ce05edd7cf8f7634fdc0f1d9aebcb78f4b51
Iñaki Sasiain Casado <inakisasiainca@gmail.com>
2023-03-18 22:10:45 +0100

    Add the UI folder

---
+++ 04_Users_interface/var_track_app.R
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c1a0985cf80f01bb40a650ce37aebeceab80efc6419c981843a2e84ec79b209a
+size 6227
---
+++ 04_Users_interface/var_track_functions.R
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b2d6f7860a6c66334fc1e0413dddb8d703ec076c1e94a3fcc4ce9082d77e98b0
+size 6293
